48a04eb25885f5f4a829f45e0244bef39ab22a28
Isaac Hinman <isaac@isaachinman.com>
2025-03-13 16:36:12 +0000

    [HYDR-11] Fix CJS resolution

--- script/build.ts
+++ script/build.ts
@@ -1,8 +1,21 @@
 import dts from 'bun-plugin-dts'
 
-await Bun.build({
+const esmBuild = Bun.build({
     entrypoints: ['./src/index.ts'],
+    format: 'esm',
     minify: true,
     outdir: './dist',
     plugins: [dts()],
 })
+
+const cjsBuild = Bun.build({
+    entrypoints: ['./src/index.ts'],
+    format: 'cjs',
+    minify: true,
+    naming: {
+        entry: '[name].cjs',
+    },
+    outdir: './dist',
+})
+
+await Promise.all([esmBuild, cjsBuild])
